b4edf4400b8f33bee4800e337d2cb126a4c86b2b
wangwencheng <960075207@qq.com>
2020-09-14 18:11:04 +0800

    删除网关无用多余文件

--- lemon-gateway/src/main/java/org/lemon/gateway/service/SystemCommonService.java
+++
@@ -1,8 +0,0 @@
-//package org.elib.auth.service;
-//
-//import org.elib.auth.dto.request.RestPwd;
-//import org.elib.common.core.util.R;
-//
-//public interface SystemCommonService {
-//	R resetPassword(RestPwd request);
-//}
